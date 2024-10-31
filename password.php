1eddb1f1c40a9c8a90adb126df2adf597d6f8220
Antognia <168127637+Antognia@users.noreply.github.com>
2024-10-31 17:47:22 +0100

    Create password.php

---
+++ password.php
@@ -0,0 +1,7 @@
+// Regole 
+// 1. la pssword lunga lameno 8 caratteri
+// 2. pssword contiene almeno un numero
+// 3. pssword contiene almeno carattere maisucolo
+// 4. pssword contiene almeno un carattere speciale
+
+$password 
