17a0608fb683c8ebe74a9cb89e68668c3c0cfa5f
Antognia <168127637+Antognia@users.noreply.github.com>
2024-10-31 18:09:44 +0100

    Update password.php

--- password.php
+++ password.php
@@ -1,7 +1,28 @@
+<?php 
+
 // Regole 
 // 1. la pssword lunga lameno 8 caratteri
 // 2. pssword contiene almeno un numero
 // 3. pssword contiene almeno carattere maisucolo
 // 4. pssword contiene almeno un carattere speciale
 
-$password 
+//readline - come il prompt e ne produce solo strinche anche se inseriamo un nuemro
+
+$password = readline('Inserisci qui la tua password');
+
+echo "password inserita:" . $password;
+
+// password lunga almeno 8 caratteri 
+if (strlen($password) >= 8) {
+echo "la password è lunga almeno 8 caratteri";
+  
+}else{
+echo "La password deve essere lunga almeno 8 caratteri";
+}
+
+// 2. pssword contiene almeno un numero
+
+if (is_numeric($password)){
+  echo " La password contiene almeno un numero";
+}else{
+  echo " La password deve contenere almeno un numero";"
