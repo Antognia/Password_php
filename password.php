d73d77a3534021c4aaef207fec1bc8059a88368e
Antognia <168127637+Antognia@users.noreply.github.com>
2024-10-31 18:42:53 +0100

    Update password.php

--- password.php
+++ password.php
@@ -20,9 +20,21 @@ echo "la password è lunga almeno 8 caratteri";
 echo "La password deve essere lunga almeno 8 caratteri";
 }
 
-// 2. pssword contiene almeno un numero
+// 2. pssword contiene almeno un numero - ENTRAMBI I METODI SONO VALIDI -
+// '/\d/' l' espressione che serve per cercare un numero (da 0 a 9);
 
-if (preg_match($password)){
+if (preg_match('/\d/', $password)){
   echo " La password contiene almeno un numero";
 }else{
-  echo " La password deve contenere almeno un numero";"
+  echo " La password deve contenere almeno un numero;"
+
+// Oppure possiamo fare così :
+    for ($i=0; $i < strlen($password); $i++){
+       if(is_numeric($password[$i])){
+         echo " La password contiene almeno un numero";
+       }else{
+          echo " La password deve contenere almeno un numero";
+       }
+
+
+  
